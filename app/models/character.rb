f25536defa75d4b7c31213b0da8c9e469f9c765a
Richard Cho <richardcho100@gmail.com>
2019-03-10 04:35:17 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -7,18 +7,6 @@ class Character < ActiveRecord::Base
   end
 
   def build_show(hash)
-    # hash.each do |key, value|
-    #   self.send("#{key}=", value)
-    #   show = Show.create(hash)
-    #
-    # end
-    # end
-
-    #create a show
-    #s
-    # show.name =
-    # self.show =
-
     show = Show.create(hash)
     self.show = show
   end
